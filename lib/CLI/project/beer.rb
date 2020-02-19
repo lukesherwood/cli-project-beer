168d607c772c76d6e83f0f9d82e4b846a2f4b0fa
Luke Sherwood <luke.sherwood55@gmail.com>
2020-02-19 02:32:34 +0000

    first commit

---
+++ lib/CLI/project/beer.rb
@@ -0,0 +1,10 @@
+require "CLI/project/beer/version"
+
+module CLI
+  module Project
+    module Beer
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/CLI/project/beer/version.rb
@@ -0,0 +1,7 @@
+module CLI
+  module Project
+    module Beer
+      VERSION = "0.1.0"
+    end
+  end
+end
